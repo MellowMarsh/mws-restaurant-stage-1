2b028c1d8efcebc7cd9aca50242a4cf614dfe78c
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2018-11-21 20:04:00 -0600

    updated service worker files

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,5 +1,28 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js');
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+// Register service worker only if supported
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log("Service Worker has been registered successfully!");
+  }).catch((e) => {
+    console.log("Couldn't register service worker... \n", e);
   });
 }
