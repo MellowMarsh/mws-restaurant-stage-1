5358be5a17e31d9a962ae2cc27915685a5734e3d
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2018-11-22 21:28:49 -0600

    made suggested changes to code from review

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let restaurants,
+restaurants,
   neighborhoods,
   cuisines
 var newMap
@@ -161,10 +161,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt=restaurant.alt;
+  image.alt=restaurant.name + 'Restaurant';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
